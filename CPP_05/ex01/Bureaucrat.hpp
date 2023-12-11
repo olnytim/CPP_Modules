14c70c60dd3de63841d7f58fbd5ca85768242b45
olnytim <yearagotomorrow@gmail.com>
2023-12-11 23:51:49 +0400

    CPP_05/ex01 is started

---
+++ CPP_05/ex01/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+using	std::exception;
+using	std::string;
+using	std::cout;
+using	std::endl;
+
+class Bureaucrat {
+private:
+	string const	_name;
+	int	_grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat( string const &name, int grade );
+	Bureaucrat( const Bureaucrat &toCopy );
+	Bureaucrat &operator =( const Bureaucrat &toCopy );
+	virtual ~Bureaucrat();
+
+	string const	&getName( void ) const;
+	int		getGrade( void ) const;
+	void	incrementGrade( void );
+	void	decrementGrade( void );
+
+	class GradeTooHighException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+};
+
+std::ostream &operator<<( std::ostream &out, const Bureaucrat &toShow );
+
+#endif
