f47b9684a8762a562e431539f44a295b98473644
olnytim <yearagotomorrow@gmail.com>
2024-01-03 17:41:12 +0400

    CPP_08/ex00 is done

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -1,6 +1,14 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
+using std::cout;
+using std::endl;
+
+template <typename T>
+int	easyfind( T &container, int value );
 
 #endif
