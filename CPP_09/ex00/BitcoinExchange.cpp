6013e0ed79bc25f102ed0d8851e8f29d80001b45
olnytim <yearagotomorrow@gmail.com>
2024-02-17 00:20:30 +0400

    starting creating CPP_09/ex00

--- CPP_09/ex00/BitcoinExchange.cpp
+++ CPP_09/ex00/BitcoinExchange.cpp
@@ -1,17 +1,4 @@
 #include "BitcoinExchange.hpp"
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-#include <fstream>
-#include <sstream>
-#include <iomanip>
 
-
-using std::endl;
-using std::cout;
-using std::string;
-using std::vector;
 
--- CPP_09/ex00/BitcoinExchange.hpp
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -1,6 +1,21 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+
+using std::endl;
+using std::cout;
+using std::string;
+using std::vector;
+
 
 
 #endif
