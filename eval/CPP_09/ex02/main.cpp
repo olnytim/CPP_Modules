91b041da5f18211da05fc76e2492801054d9a48f
olnytim <yearagotomorrow@gmail.com>
2024-03-07 14:42:45 +0400

    ex02 is started

---
+++ eval/CPP_09/ex02/main.cpp
@@ -0,0 +1,12 @@
+#include "PmergeMe.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac <= 1)
+    {
+        cerr << "Error: No input sequence provided." << endl;
+        return 1;
+    }
+//    PmergeMe pmergeMe(ac, av);
+    return 0;
+}
